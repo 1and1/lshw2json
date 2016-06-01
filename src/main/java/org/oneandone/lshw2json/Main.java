bdf31ce930bb2faf60f950371cb59fd7bb0e22f7
Stephan Fuhrmann <stephan.fuhrmann@1und1.de>
2016-06-01 15:14:42 +0200

    Json features

--- src/main/java/org/oneandone/lshw2json/Main.java
+++ src/main/java/org/oneandone/lshw2json/Main.java
@@ -5,6 +5,9 @@
  */
 package org.oneandone.lshw2json;
 
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
@@ -19,12 +22,22 @@ import org.xml.sax.SAXException;
  */
 public class Main {
 
-    
-    
+    private static JsonNode toJson(Document doc) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode result = mapper.createObjectNode();
+        return result;
+    }
+
+    private static void writeJson(File out, JsonNode json) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(out, json);
+    }
+
     public static void main(String args[]) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new File(args[0]));
-
+        JsonNode node = toJson(doc);
+        writeJson(new File(args[1]), node);
     }
 }
