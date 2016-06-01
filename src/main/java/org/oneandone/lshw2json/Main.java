53e457f6db78bf5594e4a949119f2fa1580b11f5
Stephan Fuhrmann <stephan.fuhrmann@1und1.de>
2016-06-01 12:57:55 +0200

    Rename + pom

--- src/main/java/org/oneandone/lshw2json/Main.java
+++ src/main/java/org/oneandone/lshw2json/Main.java
@@ -3,13 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.and1.lshw2json;
+package org.oneandone.lshw2json;
 
 import java.io.File;
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -17,7 +19,9 @@ import org.w3c.dom.Document;
  */
 public class Main {
 
-    public static void main(String args[]) throws ParserConfigurationException {
+    
+    
+    public static void main(String args[]) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new File(args[0]));
